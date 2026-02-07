755d3a0ca0c636e1e3a29fc7fa610cf509ee8ef3
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-02-07 23:32:41 +0530

    Time: 14 ms (24.07%), Space: 15.3 MB (6.63%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for i in range(len(nums)):
+            if nums[i] not in d:
+                d[nums[i]]=1
+            else:
+                d[nums[i]]+=1
+        for i,j in d.items():
+            if j==1:
+                return i
+        return -1
