8043cc5c771dcf0508862887d52768a64eb2697c
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-02-19 14:28:11 +0530

    Time: 11 ms (52.1%), Space: 13.7 MB (37.78%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        ans= max(d, key=d.get)
+        return ans
+        
