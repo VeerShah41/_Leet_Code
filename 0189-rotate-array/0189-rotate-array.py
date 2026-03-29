0c8d5476cd8f826ba6f6acf20c76d5716f876142
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-03-29 12:13:51 +0530

    Time: 3 ms (72.54%), Space: 22.6 MB (40.91%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,4 +1,4 @@
-from collections import deque
+
 class Solution(object):
     def rotate(self, nums, k):
         """
@@ -6,10 +6,7 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        queue = deque(nums)
         k = k % len(nums)
-        for i in range(k):
-            queue.appendleft(queue.pop())
-
-        for i in range(len(queue)):
-            nums[i]=queue[i]     
+        nums[:] = nums[-k:]+nums[:-k]
+        
+           
