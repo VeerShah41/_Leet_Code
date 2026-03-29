078be8e808b74b601d623b68e0a5f704bd7f8280
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-03-29 08:32:46 +0530

    Time: 0 ms (100%), Space: 12.3 MB (95.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,4 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        l=0
+        r=len(nums)-1
+        mid=0
+        while mid<=r:
+
+            if nums[mid]==0:
+                nums[mid],nums[l]=nums[l],nums[mid]
+                l+=1
+                mid+=1
+            elif nums[mid]==2:
+                nums[mid],nums[r]=nums[r],nums[mid]
+                r-=1
+                
+            else:
+                mid+=1
+        return nums
