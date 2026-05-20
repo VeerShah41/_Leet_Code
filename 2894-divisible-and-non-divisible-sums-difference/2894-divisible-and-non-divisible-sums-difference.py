ac40718a53bdd945b9a233ea4c6b5dab647680d2
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-05-20 22:42:35 +0530

    Time: 6 ms (34.53%), Space: 12.5 MB (19.6%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        s=0
+        for i in range(n+1):
+            if i%m==0:
+                s-=i
+            else:
+                s+=i
+        return s
