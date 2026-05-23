52beee08efb2c46d3e14f08b7fb852db0cfbcf7c
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-05-23 19:16:45 +0530

    Time: 3 ms (29.38%), Space: 12.6 MB (8.75%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        z = len(nums)
-        for i in range(z-1,-1,-1):
+        
+        for i in range(len(nums)-1,-1,-1):
             nums.append(nums[i])
         return nums
