60cdb497f05f839593026d280677c51f31515677
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-06-11 00:43:53 +0530

    Time: 8 ms (51.61%), Space: 12.4 MB (55.65%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def finalString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans = ""
+        for i in s:
+            if i=="i":
+                ans = ans[::-1]
+            else:
+                ans+=i
+        return ans
