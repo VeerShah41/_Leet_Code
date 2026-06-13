d08e06b7a2be8879b8f71894a6db2d477c73c385
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-06-13 16:34:42 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.65%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.py
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums.sort()
-        arr = []
+        
+        arr = sorted(nums)
         for i in range(0,len(nums),2):
-            arr.append(nums[i+1])
-            arr.append(nums[i])
+            arr[i],arr[i+1]=arr[i+1],arr[i]
         return arr
