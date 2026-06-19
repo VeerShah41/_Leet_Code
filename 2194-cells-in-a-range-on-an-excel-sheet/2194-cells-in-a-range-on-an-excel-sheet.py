fd62cdf70d4ebc810fe89927f9ce64c86c5fdd36
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-06-19 15:45:35 +0530

    Time: 7 ms (11.86%), Space: 12.5 MB (28.81%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.py
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.py
@@ -6,11 +6,8 @@ class Solution(object):
         """
         
         l = []
-        q = int(s[1])
-        r = int(s[-1])+1
-        start = ord(s[0])
-        end = ord(s[3])
-        for i in range(start,end+1):
-            for j in range(q,r):
+        
+        for i in range(ord(s[0]),ord(s[3])+1):
+            for j in range(int(s[1]),int(s[-1])+1):
                 l.append(chr(i)+str(j))
         return l
