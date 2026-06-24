fd4c80edd3f9f548001262fd17fad7a8a7d0d35d
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-06-24 22:35:45 +0530

    Time: 3 ms (42.19%), Space: 12.3 MB (90.63%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        place = 1
+        a = 1
         ans = 0
         while n>0:
+            
             for i in range(min(7,n)):
-                ans += (place+i)
-            place+=1
+                ans += (a+i)
+            a+=1
             n-=7
         return ans
 
