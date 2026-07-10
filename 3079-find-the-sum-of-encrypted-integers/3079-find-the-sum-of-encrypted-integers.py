f874a9d561ff95a9cc26f43a4fe3f460b6add592
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-07-10 21:29:58 +0530

    Time: 29 ms (89.55%), Space: 12.3 MB (89.55%) - LeetHub

--- 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.py
+++ 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.py
@@ -4,23 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        def encrypt(n):
-            ma = 0
-            d = 0
-            while n>0:
-                
-                ma = max(ma,n%10)
-                n=n//10
-                d+=1
-            p = 1
-            ans = 0 
-            while d>0:
-                ans += (ma*p)
-                p*=10
-                d-=1
-            return ans
-
+        
         ans = 0
         for i in nums:
-            ans+=encrypt(i)
+            ans+= int(max(str(i))*len(str(i)))
         return ans
