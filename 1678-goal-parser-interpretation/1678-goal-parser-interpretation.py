b345e1d837076904df1f6967debe7b2af95fa279
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-07-11 19:35:59 +0530

    Time: 11 ms (86.78%), Space: 12.5 MB (17.71%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -4,16 +4,4 @@ class Solution(object):
         :type command: str
         :rtype: str
         """
-        ans = ""
-        i = 0 
-        while i<len(command):
-            if command[i]=="G":
-                ans+=command[i]
-                i+=1
-            elif command[i]+command[i+1]=="()":
-                ans+="o"
-                i+=2
-            elif command[i:i+4]=="(al)":
-                ans+="al"
-                i+=4
-        return ans
+        return command.replace("()","o").replace("(al)","al")
