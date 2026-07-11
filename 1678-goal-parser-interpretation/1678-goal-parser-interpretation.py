6525efa2d286337d91023f09e3e3ea4748312ce2
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-07-11 19:34:58 +0530

    Time: 16 ms (52.37%), Space: 12.3 MB (91.02%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        ans = ""
+        i = 0 
+        while i<len(command):
+            if command[i]=="G":
+                ans+=command[i]
+                i+=1
+            elif i+1<len(command) and command[i]+command[i+1]=="()":
+                ans+="o"
+                i+=2
+            elif command[i:i+4]=="(al)":
+                ans+="al"
+                i+=4
+        return ans
