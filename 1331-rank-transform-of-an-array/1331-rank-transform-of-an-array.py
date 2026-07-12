26ca15c0819d0a30359d0514c8945f68f5ae3a84
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-07-12 11:13:15 +0530

    Time: 44 ms (65.02%), Space: 27.9 MB (77.37%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -4,7 +4,5 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-        temp = sorted(set(arr))
-        d = {v:i+1 for i,v in enumerate(temp)}
-        res = [d[x] for x in arr]
-        return res
+        d = {v:i+1 for i,v in enumerate(sorted(set(arr)))}
+        return [d[x] for x in arr]
