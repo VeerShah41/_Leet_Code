164aea6231c034647eec5b71302a34503f7737ea
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-07-12 11:20:33 +0530

    Time: 4 ms (44.72%), Space: 12.4 MB (19.51%) - LeetHub

--- 3813-vowel-consonant-score/3813-vowel-consonant-score.py
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.py
@@ -5,14 +5,17 @@ class Solution(object):
         :rtype: int
         """
         l = ['a', 'e', 'i', 'o', 'u']
+        n = [" ", "1","2","3","4","5","6","7","8","9","0"]
         v = 0
         c = 0
         for i in s:
-            if i.isalpha():
-                if i in l:
-                    v+=1
-                else:
-                    c+=1
+            
+            if i in l:
+                v+=1
+            elif i in n:
+                continue
+            else:
+                c+=1
         if c==0:
             return 0
         return v//c
