d96619fcd4d369cd9260d676d63d10e708cc61e0
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-07-13 22:28:00 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.05%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,13 @@
+from math import gcd
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        g = gcd(a,b)
+        count =0
+        for i in range(1,int(g**0.5)+1):
+            if g%i==0:
+                count+=1
+                if i!=g//i:
+                    count+=1
+
+
+        return count
