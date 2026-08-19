d9f375ee18c0696ad01ee9f27cc8698f782fddbd
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-08-19 15:41:52 +0530

    Time: 0 ms (100%), Space: 12.5 MB (1.91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,25 +5,25 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        
+        ans = [ [1] , [1, 1] ]
         if numRows==1:
-            return [[1]]
-        ans = [[1],[1,1]]
-        flag = 3
+            return [ans[0]]
         if numRows==2:
             return ans
-        def help(flag,ans):
-            l=[1]
-
-            for i in range(flag-2):
-                l.append(ans[i]+ans[i+1])
+        
+        def helper(row,arr):
+            l = [1]
+            for i in range(1,len(arr)):
+                l.append(arr[i-1]+arr[i])
             l.append(1)
-            
             return l
-        for i in range(2,numRows):
-            ans.append(help(flag,ans[-1]))
-            flag+=1
+
+        for i in range(3,numRows+1):
+            ans.append(helper(i,ans[i-2]))
+
         return ans
+        
+        
 
             
 
