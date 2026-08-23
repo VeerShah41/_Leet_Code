b5e45dcfcc5f7cd2baa2b6577ae01db5c43903d7
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-08-23 23:00:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (42.42%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        n = len(mat)
+        ans = 0
+        for i in range(n):
+            ans+=mat[i][i]
+            ans+=mat[i][n-1-i]
+        if n%2:
+            ans-=mat[n//2][n//2]
+        return ans
+                
+            
