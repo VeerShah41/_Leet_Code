f2f820c8378a315d40aa3a2dcbea115452c34c45
Veer Shah <185037322+VeerShah41@users.noreply.github.com>
2026-09-05 13:52:21 +0530

    Time: 319 ms (80.65%), Space: 22.9 MB (29.03%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -6,19 +6,13 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-
-        suffix = [0] * n
-        suffix[-1] = nums[-1]
-
-        for i in range(n - 2, -1, -1):
-            suffix[i] = min(nums[i], suffix[i + 1])
-
-        ma = float("-inf")
-
-        for i in range(n):
-            ma = max(ma, nums[i])
-
-            if ma - suffix[i] <= k:
-                return i
-
+        suffix = [0]*n
+        suffix[-1]=nums[-1]
+        for i in range(n-2,-1,-1):
+            suffix[i]=min(nums[i],suffix[i+1])
+        ma = 0
+        for j in range(n):
+            ma = max(ma,nums[j])
+            if ma - suffix[j] <= k:
+                return j
         return -1
